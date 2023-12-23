6f679fb9013219c5e8f125e54a256a171ea28473
76500833 <145300587+76500833@users.noreply.github.com>
2023-12-22 20:51:47 -0500

    coords now fetch weather

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,34 @@
 const key = "78c13c7d14026703c4632a7298ef5634"
 
 
-function getCoords(city, state) {
-
-    let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}"&limit=5&appid=${key}`
+function getData(city, state) {
+    //* Get Coords
+    let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}"&limit=1&appid=${key}`
     fetch(requestUrl)
     .then(function (response) {
     return response.json();
+ 
     })
     .then(function (data) {
         console.log(data)
+        let long = data[0].lon
+        let lat = data[0].lat
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}`
+        
+        //* Use the coords to get weather info
+        fetch(weatherUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
     })
     
-
 }
 $("#searchButton").on("click", function(){
     let city = $("#searchCity").val()
     let state = $("#searchState").val()
-    console.log(city)
-    console.log(state)
-    getCoords(city, state)
+    getData(city, state)
+
 })
