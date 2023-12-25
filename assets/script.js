9e24f976dc42ffe717fb5dc06210d3159f945c97
76500833 <145300587+76500833@users.noreply.github.com>
2023-12-25 09:11:30 -0500

    appends name, now

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
 const key = "78c13c7d14026703c4632a7298ef5634"
 
+let bodyEl = $('body')
+
+let divForSearch = $('<div>')
+ 
+bodyEl.append(divForSearch)
+
 
 function getData(city, state) {
-    //* Get Coords
+//& Get Coords
     let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}"&limit=1&appid=${key}`
     fetch(requestUrl)
     .then(function (response) {
@@ -14,18 +20,25 @@ function getData(city, state) {
         let long = data[0].lon
         let lat = data[0].lat
         let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}`
-        
-        //* Use the coords to get weather info
         fetch(weatherUrl)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
             console.log(data)
+            let cardContainer = $('<div>').attr("id", "cardContainer")
+            let dayOneDateText = (data.city.name)
+            console.log(dayOneDateText)
+            let dayOne = $('<div>').text(dayOneDateText)
+            cardContainer.append(dayOne)
+            bodyEl.append(cardContainer)
         })
     })
     
 }
+
+
+//TODO save search history in local storage
 $("#searchButton").on("click", function(){
     let city = $("#searchCity").val()
     let state = $("#searchState").val()
